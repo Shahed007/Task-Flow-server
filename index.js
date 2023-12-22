144fd672a8340e295183f3f2851945a39e2d72db
Shahed <sk0178397@gmial.com>
2023-12-22 11:09:23 +0600

    Created structure file.

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.77jbz4j.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const taskFlowDB = client.db("taskFlowDB");
+    const taskCollection = taskFlowDB.collection("taskCollection");
+
+    app.post("task", async (req, res) => {
+      try {
+        const task = req.body;
+        const result = await taskCollection.insertOne(task);
+        res.send(result);
+      } catch (error) {
+        res.status(200).send({ message: "internal server error" });
+      }
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("my Home Care Hub server is running");
+});
+
+app.listen(port, () => {
+  console.log(`my server is running port ${port}`);
+});
